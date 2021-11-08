b1239f9536b7ddd0b00dfdfed8fa2e22a6ad10fa
OzanSahin92 <ozansahin92@gmail.com>
2021-11-08 22:29:44 +0100

    main.cpp refactored

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,6 @@ int main(int argc, char *argv[]) {
 
     manager.work();
 
-    //TwentyTwentyTwentyTimer TwentyTwentyTwentyTimer(1, 1, true);
-    //TwentyTwentyTwentyTimer.twentyTwentyTwentyTimer();
-
-    //auto app = Gtk::Application::create(argc, argv,"org.gtkmm.examples.base");
-
-    //const string title = "20 20 20";
-    //const string label = " 20 Sekunden lang auf einen 20 Fuß (ca. 6m) entfernten Punkt gucken, \n um die Augen zu entspannen! ";
-    //const int height = 400;
-    //const int width = 400;
-
-    //TwentyTwentyTwentyWindow window(title, label, height, width);
-    //app->run(window);
-
     delete window;
     return EXIT_SUCCESS;
 }
